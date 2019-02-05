42e9799d8b69273310277b27bc1f875868052c65
dnesbitt03qub <dnesbitt03@qub.ac.uk>
2019-02-05 17:17:45 +0000

    Fill out subtitle loader

--- subtitle_word_loader.py
+++ subtitle_word_loader.py
@@ -1,2 +1,17 @@
+import srt
+from typing import List
+
 def get_words_from_subtitle_file(path: str) -> List[str]:
     
+    # Read from file
+    with open(path, 'r') as file:
+        subtitle_file = file.read()
+    
+    # Get content from file
+    subtitle_generator = srt.parse(subtitle_file)
+    subtitles = list(subtitle_generator)
+    
+    # Put content into List of str
+    subtitle_content = [subtitle.content for subtitle in subtitles]
+    
+    return subtitle_content
