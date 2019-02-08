087da74cbfb13cffc6779a3dd5ff84061f6cbf78
dnesbitt03qub <dnesbitt03@qub.ac.uk>
2019-02-08 17:25:50 +0000

    Filling in evaluation mode

--- eval_mode.py
+++ eval_mode.py
@@ -1,5 +1,6 @@
 import game_data
 import subtitle_word_loader as swl
+import results
 
 def evaluate_game(game_file_path: str, subtitle_path: str):
     # Load game file from the path
@@ -9,13 +10,14 @@ def evaluate_game(game_file_path: str, subtitle_path: str):
     # Get the list of words from the subtitle file
     words_from_subtitle_file = swl.get_words_from_subtitle_file(subtitle_path)
     
-    print(words_from_subtitle_file)
-    
     # Get the list of words that the players have guessed
     guessed_words - game.get_all_words()
     
     # Set counts for each of the guessed words
-    # Set those counts for each player
+    word_counts = counts_for_each_guessed_word(guessed_words)
+    most_common_words = most_common_words_with_count(words_from_subtitle_file)
+    
     # Extension: Get the top 10 words and fill in
     
     # Display the results all fancy
+    results.display(game, word_counts, most_common_words)
